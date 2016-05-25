a268b459b5f96d4002f58826593e5a2e7a66392b
HeroDishonest <xanderxander2015@yandex.ru>
2016-05-25 22:45:19 +0300

    25.05

--- EmployeeTest.java
+++ EmployeeTest.java
@@ -9,7 +9,7 @@ import java.util.GregorianCalendar;
 public class EmployeeTest {
 
     public static void main(String[] args) {
-        Employee[] staff = new Employee[3];
+        Employee[] staff = new Employee[4];
         staff[0] = new Employee("Carl Cracker",75000, 1987, 12, 15);
         staff[1] = new Employee("Harry Hacker",50000, 1989, 10, 1);
         staff[2] = new Employee("Tony Tester",40000, 1990, 3, 15);
