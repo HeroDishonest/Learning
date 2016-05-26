0150159a0e5c556ba48b12cd616c1c1f4f6bba94
i.heldyieu <xanderxander2015@yandex.ru>
2016-05-26 16:13:36 +0300

    1

--- EmployeeTest.java
+++ EmployeeTest.java
@@ -4,14 +4,16 @@ public class EmployeeTest {
 
     public static void main(String[] args) {
 
+        Manager boss = new Manager("Carl Cracker", 80000,1987, 10, 1);
+        boss.setBonus(5000);
         Employee[] staff = new Employee[3];
-        staff[0] = new Employee("Carl Cracker",75000, 1987, 12, 15);
+        staff[0] = boss;
         staff[1] = new Employee("Harry Hacker",50000, 1989, 10, 1);
         staff[2] = new Employee("Tony Tester",40000, 1990, 3, 15);
 
         for (Employee w : staff){
             w.raiseSalary(5);
-            System.out.println("name = "+ w.getName() +", salary = "+ w.getSalary()+", hireDay = "+w.getHireDay());
+            System.out.println("name = "+ w.getName() +", salary = "+ w.getSalary());
         }
     }
 
---
+++ Manager.java
@@ -0,0 +1,20 @@
+/**
+ * Created by i.heldyieu on 26.05.2016.
+ */
+public class Manager extends Employee{
+    private double bonus;
+
+    public Manager (String n, double s, int year, int month, int day){
+        super(n,s,year,month,day);
+        bonus =0;
+    }
+
+    public double getSalary(){
+        double baseSalary = super.getSalary();
+        return baseSalary+bonus;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+}
